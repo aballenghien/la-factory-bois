1adfb749a66bd9a429d0fc24f1a7b7a68f6bfee2
aballenghien <audrey.ballenghien@gmail.com>
2020-02-07 13:31:15 +0100

    Création du projet boot

---
+++ lafactory-bossfinal/src/main/java/fr/lafactory/LafactoryBossfinalApplication.java
@@ -0,0 +1,13 @@
+package fr.lafactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LafactoryBossfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LafactoryBossfinalApplication.class, args);
+	}
+
+}
---
+++ lafactory-bossfinal/src/test/java/fr/lafactory/LafactoryBossfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lafactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LafactoryBossfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
