3691f23d51c489a5aea1c76aacd28b99ac1dbe90
plmairet <pl.mairet@gmail.com>
2020-02-10 17:33:53 +0100

    Angular page des étapes

---
+++ lafactory-angular/src/app/etape/etape.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {AppConfigService} from '../service//app-config.service'
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-etape',
+  templateUrl: './etape.component.html',
+  styleUrls: ['./etape.component.css']
+})
+export class EtapeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- lafactory-angular/src/app/service/etape.service.ts
+++ lafactory-angular/src/app/service/etape.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {AppConfigService} from '../service//app-config.service'
+import { Etape } from '../model/Etape'
+import { Modele } from '../model/Modele'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EtapeService {
+  public etapes:Array<Etape> =null;
 
-  constructor() { }
+  constructor(private http: HttpClient, private srvAppConfig: AppConfigService) { }
+
+  
+if(Modele.id != 0){
+  this.http.get<Array<Etape>>(`${this.srvAppConfig.url}etape`).subscribe(resp => this.etapes = resp );
+}
+
+
+public findAll() : Array<Etape>{
+  this.http.get<Array<Etape>>(`${this.srvAppConfig.url}etape`).subscribe(resp => this.etapes = resp );
+  return this.etapes;
+}
+
+
+
+
+
 }
