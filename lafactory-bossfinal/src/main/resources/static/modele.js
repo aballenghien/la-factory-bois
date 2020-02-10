38437d5628db502451455dd9183e1943377e1c05
aballenghien <audrey.ballenghien@gmail.com>
2020-02-10 15:00:39 +0100

    Ajout des catégories dans le modèle

--- lafactory-bossfinal/src/main/resources/static/modele.js
+++ lafactory-bossfinal/src/main/resources/static/modele.js
@@ -1,9 +1,13 @@
 function ajouterCategorie() {
-	let elementListCategories=document.getElementById("categorie");
-	let idCategorie=elementListCategories.options[elementListCategories.selectedIndex].value;
-	let nomCategorie=elementListCategories.options[elementListCategories.selectedIndex].text;
-	let textArea=document.getElementById("listeDesCategories");
-	if(idCategorie != 0){
-		textArea.value = textArea.value+idCategorie+":"+nomCategorie+"\n";
+	let elementListCategories = document.getElementById("categorie");
+	let idCategorie = elementListCategories.options[elementListCategories.selectedIndex].value;
+	let nomCategorie = elementListCategories.options[elementListCategories.selectedIndex].text;
+	let textArea = document.getElementById("listeDesCategories");
+
+	if (idCategorie != 0) {
+		if (!textArea.value.includes(idCategorie + ":" + nomCategorie)) {
+			textArea.value = textArea.value + idCategorie + ":" + nomCategorie
+					+ "\n";
+		}
 	}
 }
