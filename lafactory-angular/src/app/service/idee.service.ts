e86dede9cd585b6290f2fedcaeb9b88984238c52
Adrian <adrian.oliva@soprasteria.com>
2020-02-10 15:57:39 +0100

    Angular Boite Idee

---
+++ lafactory-angular/src/app/service/idee.service.ts
@@ -0,0 +1,22 @@
+import { Idee } from '../model/idee';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AppConfigService } from './app-config.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IdeeService {
+
+  constructor(private httpClient : HttpClient, private srvAppConfig: AppConfigService) { }
+
+  public idees: Array<Idee> = null;
+  // private idee : Idee = new Idee();
+
+  public send (idee : Idee):void{
+    console.log(idee);
+      this.httpClient
+        .post<Idee>(`${ this.srvAppConfig.url + 'idee' }`, idee)
+        .subscribe(resp => this.idees.push(resp));
+  }
+}
---
+++ lafactory-angular/src/app/service/visiteur.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Visiteur } from '../model/visiteur';
+import { HttpClient } from '@angular/common/http';
+import { AppConfigService } from './app-config.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VisiteurService {
+
+  constructor(private httpClient : HttpClient, private srvAppConfig: AppConfigService) { }
+
+  public visiteurs: Array<Visiteur> = null;
+
+  public save (visiteur : Visiteur):void{
+    console.log(visiteur);
+      this.httpClient
+        .post<Visiteur>(`${ this.srvAppConfig.url + 'visiteur' }`, visiteur)
+        .subscribe(resp => this.visiteurs.push(resp));
+  }
+}
