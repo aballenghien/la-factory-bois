49074ab8e7011077ce3df15d4f6223053a11d2a3
marcellinsimon <marcellin.simon@soprasteria.com>
2020-02-10 10:54:18 +0100

    rectif du mapping

--- lafactory-bossfinal/src/main/java/fr/lafactory/model/Utilisateur.java
+++ lafactory-bossfinal/src/main/java/fr/lafactory/model/Utilisateur.java
@@ -7,13 +7,13 @@ import javax.validation.constraints.NotEmpty;
 
 @Entity
 @DiscriminatorValue("utilisateur")
-public class Utilisateur extends Visiteur {
+public class Utilisateur extends Personne {
 	
-	@Column (name = "VIS_USERNAME", nullable = false, length = 100)
+	@Column (name = "UTI_USERNAME", nullable = false, length = 100)
 	@NotEmpty(message = "Veuillez entrer votre username")
 	private String username;
 	
-	@Column(name = "VIS_PASSWORD", length = 500)
+	@Column(name = "UTI_PASSWORD", length = 500)
 	private String password;
 
 	public String getUsername() {
--- lafactory-bossfinal/src/main/java/fr/lafactory/model/Visiteur.java
+++ lafactory-bossfinal/src/main/java/fr/lafactory/model/Visiteur.java
@@ -1,92 +1,10 @@
 package fr.lafactory.model;
 
-import java.util.List;
-
-import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "visiteur")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name="UTI_TYPE")
-public class Visiteur {
+@DiscriminatorValue("visiteur")
+public class Visiteur extends Personne {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "UTI_ID")
-	protected int id;
-	
-	@Column(name = "UTI_NOM", length = 100)
-	protected String nom;
-	
-	@Column(name = "UTI_PRENOM", length = 100)
-	protected String prenom;
-	
-	@Column(name = "UTI_MAIL", length = 100)
-	protected String mail;
-	
-	@OneToMany(mappedBy = "visiteur")
-	protected List<Idee> idees;
-	
-	@Column(name ="UTI_ROLE",nullable = false)
-	@Enumerated(EnumType.STRING)
-	protected Role role = Role.VISITEUR;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public String getPrenom() {
-		return prenom;
-	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-	public String getMail() {
-		return mail;
-	}
-
-	public void setMail(String mail) {
-		this.mail = mail;
-	}
-
-	public List<Idee> getIdees() {
-		return idees;
-	}
-
-	public void setIdees(List<Idee> idees) {
-		this.idees = idees;
-	}
-
-	public Role getRole() {
-		return role;
-	}
-
-	public void setRole(Role role) {
-		this.role = role;
-	}
 }
