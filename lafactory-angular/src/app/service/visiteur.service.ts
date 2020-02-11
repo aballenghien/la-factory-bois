4d2cfcfefc0e6e9dbf2c287ceb400c06360d407b
plmairet <pl.mairet@gmail.com>
2020-02-11 12:28:11 +0100

    Auto stash before merge of "AffichageEtape" and "master"

--- lafactory-angular/src/app/service/visiteur.service.ts
+++ lafactory-angular/src/app/service/visiteur.service.ts
@@ -1,4 +1,4 @@
-/*import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Visiteur } from '../model/visiteur';
 import { HttpClient } from '@angular/common/http';
 import { AppConfigService } from './app-config.service';
@@ -19,4 +19,3 @@ export class VisiteurService {
         .subscribe(resp => this.visiteurs.push(resp));
   }
 }
-*/
